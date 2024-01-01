c97a4c878d521dc97c3421fa28b4a424b6829f66
gratachu <akitatsu95.climbing@gmail.com>
2024-01-01 19:12:09 +0900

    Add: Chapter13 error handling to updateInvoice and deleteInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -50,17 +50,30 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
-  `;
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to updateInvoice.',
+    }
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Invoice deleted successfully.' };
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to deleteInvoice.',
+    }
+  }
 }
